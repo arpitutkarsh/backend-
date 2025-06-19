613583acfa24c4e306618aa500941dfc19db6e1b
Arpit <arpit.utkarsh49@gmail.com>
2025-06-19 16:51:25 +0530

    Created User model, Video Model in video model wrote the code for videooSchema.plugin(mongooseAggregatePaginate) in user model used bcrypt to encrypt and ecrypt the password

---
+++ src/models/user.model.js
@@ -0,0 +1,68 @@
+import mongoose, {Schema} from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, //it will be URL from Cloudinary
+        required: true,
+    },
+    coverImage: {
+        type: String,
+    },
+    watchHistory: [{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }],
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String
+    }
+
+}, {timestamps: true});
+
+//the below code will execute everytime a user updates data, but we want it to execute only when the password field is modified
+/*
+userSchema.pre("save", async function(next) {
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+*/
+//so now we will use if to check if the password is modified or not
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+//like pre, post we can create our own method using scehmaName.methods.Method_name
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)  //we are using await here because decrypting takes time
+}
+export const User = mongoose.model("User", userSchema);
