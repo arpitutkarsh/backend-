8acaaf8a84270d8b788282f02427e942b83e420c
Arpit <arpit.utkarsh49@gmail.com>
2025-06-21 20:36:16 +0530

    Checked if we can create register or login function without asyncHandler,
    Generated access and refresh tokens,
    
    written functionality for loginuser and then exported both registerUser and lginUser
    
    (previous commit--not necessary)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,31 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {User} from '../models/user.model.js'
 import {uploadonCloudinary} from "../utils/cloudinary.js"
-const registerUser = asyncHandler( async (req, res) => {
+import { response } from "express";
+//we can also use asynch handler for below codes like
+/*
+const registerUser = asyncHandler(async(req, res) => {
+
+})
+*/
+
+const generateAccessandRefreshToken = async (userId) => {
+    try{
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})  //so before saving it will also require all the data again to disable this we need validateBeforeSave to be false
+
+        return {accessToken, refreshToken}
+
+
+    }
+    catch(error){
+        throw new ApiError(501, "Smething went wrong")
+    }
+}
+const registerUser =  async (req, res) => {
     //get user details from frontend
     //validation like if any field is empty, user inputs the data in correct format etc etc
     //check if user already exists: CHeck using username and email also
@@ -90,7 +114,64 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
 
-} )
+}
+
+const loginUser = async(req, res) => {
+    //take input from user - use req body to get data
+    //check validation
+    //find the user
+    //if user exists check for password
+    //if password is correct generate accessToken and refreshToken
+    //send cookies
+    //response
+
+    const {email, username, password} = req.body
+    if(!username || !email){
+        throw new ApiError(400, "Username or Email is required")
+    }
+
+    //now find the user
+    const user = await User.findOne({ //instead of findOne we can also use find
+        $or: [{username}, {email}]  //we are using $or because we want to find either username or email
+    })
+
+    if(!user){
+        throw new ApiError(404, "User does not exist")
+    }
+
+    //now checking for password
+    const validPassword = await user.isPasswordCorrect(password)  //to access methods defined by us in our userModel we use user (with small u)
+    //to access the methods available in MONGODB we use User(with capital U)
+
+    if(!validPassword){
+        throw new ApiError(401, "Passwrod is Incorrect")
+    }
+    //create access and refresh token
+    const {accessToken, refreshToken} = await generateAccessandRefreshToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken") //as we dont want to send password and refreshToken to the user
+
+    //send accessToken and refreshToken via cookies
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse
+        (200,
+            {
+            user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged in successfully"
+        )
+    )
+}
 
 
-export {registerUser};
+
+
+export {registerUser, loginUser};
