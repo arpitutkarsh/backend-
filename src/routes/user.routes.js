411b226f2768792a80126b8d9b9b903417892700
Arpit <arpit.utkarsh49@gmail.com>
2025-06-25 15:08:07 +0530

    Imported RefreshAccessToken function to user.route.js file and then written database model for subscriptions

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"; 
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 //to handle file we need to import upload from multer.middleware.js
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -22,5 +22,6 @@ router.route("/login").post(loginUser)
 
 //checking if the user is logged in or not
 router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
